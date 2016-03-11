35c57a38a9905bda66434edaf2d7ea50e6e3c038
Francisco Navarro Madueño <frannm323@hotmail.com>
2016-03-11 20:50:13 +0100

    Have added a different looking to MusicVideo class with a class method called retrievedValueFromChain

--- MusicVideo/MusicVideo.swift
+++ MusicVideo/MusicVideo.swift
@@ -12,19 +12,29 @@ class Video{
     
     
     private struct APIVideoConstants{
-          static let ImageLabelName = "im:name"
-          static let ImageLabel = "label"
-          static let VideoImageName = "im:image"
-          static let VideoLabel = "label"
-          static let VideoUrlLink = "link"
-          static let VideoUrlAtrributes = "attributes"
-          static let VideoUrlHref = "href"
+          static let imageName = "im:name/label"
+          static let rights = "rights/label"
+          static let price = "im:price/label"
+          static let imageURL = "im:image/2/label"
+          static let artist = "im:artist/label"
+          static let videoURL = "link/1/attributes/href"
+          static let iMid = "id/attributes/im:id"
+          static let genre = "category/attributes/term"
+          static let linkToiTunes = "id/label"
+          static let releaseDate = "im:releaseDate/attributes/label"
     }
     //Data encapsulation
     
     private var _vName:String
+    private var _vRights:String
+    private var _vPrice:String
     private var _vImageUrl:String
+    private var _vArtist:String
     private var _vVideoUrl:String
+    private var _vImid:String
+    private var _vGenre:String
+    private var _vLinkToiTunes:String
+    private var _vReleaseDte:String
     
     //Make a getter
     
@@ -40,36 +50,60 @@ class Video{
         return _vVideoUrl
     }
     
+    
+    class func retrieveValueFromChain(chain:String, data:AnyObject)->String{
+        
+        var stringValue = ""
+        
+        var chainDivided = chain.componentsSeparatedByString("/")
+        
+        if (chainDivided.count>0)
+        {
+            
+            let newPart = chainDivided.removeLast()
+            let dataExtracted = data[newPart]
+            
+            switch dataExtracted{
+            case let value as JSONDictionary:
+                retrieveValueFromChain(chainDivided.joinWithSeparator("/"),data: value)
+            case let value as JSONArray:
+                if (chainDivided.count>0){
+                    let numberArray = chainDivided.removeLast()
+                    if let numberFromString = Int(numberArray){
+                        let newData = value[numberFromString]
+                        retrieveValueFromChain(chainDivided.joinWithSeparator("/"),data: newData)
+                    }
+                }
+            case let value as String:
+                stringValue = value
+            default:
+                stringValue = ""
+            }
+        }
+        
+        return stringValue
+    }
+
+    
     init (data: JSONDictionary){
         
+        let datos: JSONDictionary = data
+        //set imageName
+        _vName = Video.retrieveValueFromChain(APIVideoConstants.imageName, data: datos)
+        //set imageURL
+        let imageUrl = Video.retrieveValueFromChain(APIVideoConstants.imageURL, data: data)
+        _vImageUrl = imageUrl.stringByReplacingOccurrencesOfString("100x100", withString: "600x600")
+        //set videoURL
+        _vVideoUrl = Video.retrieveValueFromChain(APIVideoConstants.videoURL, data: data)
         
-        if let name = data[APIVideoConstants.ImageLabelName] as? JSONDictionary, vName = name[APIVideoConstants.ImageLabel] as? String{
-            self._vName = vName
-        }else{
-            _vName = ""
-        }
-        
-        if let img = data[APIVideoConstants.VideoImageName] as? JSONArray,
-               image = img[2] as? JSONDictionary,
-            immage = image[APIVideoConstants.VideoLabel] as? String{
-                _vImageUrl = immage.stringByReplacingOccurrencesOfString("100x100", withString: "600x600")
-                
-        }else{
-            _vImageUrl = ""
-        }
-        
-        if let video = data[APIVideoConstants.VideoUrlLink] as? JSONArray,
-               vUrl = video[1] as? JSONDictionary,
-               vHref = vUrl[APIVideoConstants.VideoUrlAtrributes] as? JSONDictionary,
-            vVideoUrl = vHref[APIVideoConstants.VideoUrlHref] as? String {
-                self._vVideoUrl = vVideoUrl
-        }else{
-            _vVideoUrl = ""
-        }
-        
-        
-        
-        
+        //set the homework's variables
+        _vRights = Video.retrieveValueFromChain(APIVideoConstants.rights, data: data)
+        _vPrice = Video.retrieveValueFromChain(APIVideoConstants.price, data: data)
+        _vArtist = Video.retrieveValueFromChain(APIVideoConstants.artist, data: data)
+        _vImid = Video.retrieveValueFromChain(APIVideoConstants.iMid, data: data)
+        _vGenre = Video.retrieveValueFromChain(APIVideoConstants.genre, data: data)
+        _vLinkToiTunes = Video.retrieveValueFromChain(APIVideoConstants.linkToiTunes, data: data)
+        _vReleaseDte = Video.retrieveValueFromChain(APIVideoConstants.releaseDate, data: data)
         
     }
     
