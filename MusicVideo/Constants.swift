5ebab3c996232affc2577f8700b828d0c7beb8a5
Francisco Navarro Madueño <frannm323@hotmail.com>
2016-03-13 20:44:53 +0100

    Part-7 reachability files added and bridge header prepared

--- MusicVideo/Constants.swift
+++ MusicVideo/Constants.swift
@@ -15,3 +15,23 @@ typealias JSONArray = Array <AnyObject>
 public struct API {
     static let DefaultsKey = "https://itunes.apple.com/us/rss/topmusicvideos/limit=10/json"
 }
+
+enum InternetStatus: CustomStringConvertible{
+    case WIFI
+    case NOACCESS
+    case WWAN
+    
+    var description: String{
+            get {
+                switch self {
+                case .WIFI:
+                    return "WIFI available"
+                case .NOACCESS:
+                    return "No internet Access"
+                case .WWAN:
+                    return "Cellular Access Available"
+                }
+            }
+        
+    }
+}
